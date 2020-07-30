a4e4910ed180a1f0204595130615f079e1e3e6ed
oleksii.doroshenko <oleksii.doroshenko@teamdev.com>
2020-07-30 13:17:47 +0300

    Add unit test for user model

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -1,13 +1,25 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import {module, test} from 'qunit';
+import {setupTest} from 'ember-qunit';
 
-module('Unit | Model | user', function(hooks) {
+const modelName = 'user';
+const attributeSchema = {
+  name: 'string',
+  books: '-mf-fragment-array$book',
+}
+
+
+module('Unit | Model | user', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let model = store.createRecord('user', {});
-    assert.ok(model);
+  test('model attributes must have expected data types', async function (assert) {
+
+    let model = this.owner.lookup('service:store').modelFor(modelName);
+
+    let actualAttributeSchema = {}
+    model.eachAttribute((fieldName, meta) => {
+      actualAttributeSchema[fieldName] = meta.type;
+    })
+    assert.deepEqual(actualAttributeSchema, attributeSchema, 'Actual attributes schema should match expected');
   });
+
 });
